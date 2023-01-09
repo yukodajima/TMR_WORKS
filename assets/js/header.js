33a4f5c2d4822bd67bf89212a196c2e5ddfb4c15
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2023-01-09 14:06:12 +0900

    ヘッダーjquery修正

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,7 +2,12 @@ $(window).on('load', function () {
   jQuery(function ($) {
     console.log("ok");
    $(".p-header__menu").on("click", function(){
-    $(".p-header__menuList").addClass("open")
+    $(".p-header__menuList").toggleClass("open")
+    $(".p-header__menuList").slideToggle()
    })
+   $('.p-header__drawerMenu').on('click', function () {
+    $(".p-header__drawerMenuItem").toggleClass('open');
+    $('.p-header__shopList').slideToggle();
+  });
   });
 });
