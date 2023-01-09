35d926b6f5c7a10516252dff1e5c7c60927a5401
yukodajima <koda12131107@gmail.com>
2023-01-09 15:02:02 +0900

    .p-topMenuのjs追加

--- assets/js/changeTab.js
+++ assets/js/changeTab.js
@@ -1,13 +1,20 @@
 $(window).on('load', function () {
   jQuery(function ($) {
-    const changeTab = $('.p-topMenu__textContainer').on('click', function () {
-      $('.p-topMenu__textContainer').removeClass('orange');
-      $('.p-topMenu__title').removeClass('white');
-      $('.p-topMenu__subTitle').removeClass('white');
-      let index = $('.p-topMenu__textContainer').index(this);
-      $('.p-topMenu__textContainer').eq(index).addClass('orange');
-      $('.p-topMenu__title').eq(index).addClass('white');
-      $('.p-topMenu__subTitle').eq(index).addClass('white');
+    let tab = $('.p-topMenu__textContainer');
+    let title = $('.p-topMenu__title');
+    let subTitle = $('.p-topMenu__subTitle');
+    let parent = $('.l-container');
+    let list = $('.p-topMenu__list');
+    const changeTab = tab.on('click', function () {
+      tab.removeClass('orange');
+      title.removeClass('white');
+      subTitle.removeClass('white');
+      let index = tab.index(this);
+      tab.eq(index).addClass('orange');
+      title.eq(index).addClass('white');
+      subTitle.eq(index).addClass('white');
+      $(this).closest(parent).find(list).removeClass('show');
+      $(this).closest(parent).find(list).eq($(this).index()).addClass('show');
     });
   });
 });
