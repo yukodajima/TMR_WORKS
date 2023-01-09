756663230c129f4763f38063c78052102c402915
yukodajima <koda12131107@gmail.com>
2023-01-09 20:44:07 +0900

    repair.htmlの修正、jsの追加

--- assets/js/changeTab.js
+++ assets/js/changeTab.js
@@ -16,5 +16,21 @@ $(window).on('load', function () {
       $(this).closest(parent).find(list).removeClass('show');
       $(this).closest(parent).find(list).eq($(this).index()).addClass('show');
     });
+    let tab2 = $('.p-menu__textContainer');
+    let title2 = $('.p-menu__title');
+    let subTitle2 = $('.p-menu__subTitle');
+    let parent2 = $('.l-container');
+    let list2 = $('.p-menu__list');
+    const changeTab2 = tab2.on('click', function () {
+      tab2.removeClass('orange');
+      title2.removeClass('white');
+      subTitle2.removeClass('white');
+      let index = tab2.index(this);
+      tab2.eq(index).addClass('orange');
+      title2.eq(index).addClass('white');
+      subTitle2.eq(index).addClass('white');
+      $(this).closest(parent2).find(list2).removeClass('show');
+      $(this).closest(parent2).find(list2).eq($(this).index()).addClass('show');
+    });
   });
 });
