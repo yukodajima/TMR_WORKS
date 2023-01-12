b66ffd7ddba47dd19aa4142544272ef7319e6677
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2023-01-12 23:24:41 +0900

    contactページ作成

---
+++ header.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css" />
+    <?php wp_head(); ?>
+  </head>
+
+  <body>
+    <div class="l-wrapper">
+      <header class="l-header">
+        <div class="p-header">
+          <div class="p-header__titleBox">
+            <div class="p-header__logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-pc.jpg" alt=""></div>
+            <div class="p-header__menuArea">
+              <div class="p-header__btnArea">
+                <button class="c-btn p-header__btn">店舗一覧</button>
+                <button class="c-btn__contact p-header__contactBtn">お問い合わせ</button>
+              </div>
+              <div class="p-header__menu">
+                <div class="p-header__menuLine"></div>
+                <div class="p-header__menuLine"></div>
+                <div class="p-header__menuLine"></div>
+              </div>
+            </div>
+          </div>
+          <ul class="p-header__menuList">
+            <li class="p-header__menuItem">
+              <a class="p-header__menuLink" href="">
+                <div class="p-header__menuLogo">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icn-twitter.svg" alt="">
+                </div>
+                <p class="p-header__menuText">HOME</p>
+              </a>
+            </li>
+            <li class="p-header__menuItem">
+              <a class="p-header__menuLink" href="">
+                <div class="p-header__menuLogo">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icn-twitter.svg" alt="">
+                </div>
+                <p class="p-header__menuText">初めての方へ</p>
+              </a>
+            </li>
+            <li class="p-header__menuItem">
+              <a class="p-header__menuLink" href="">
+                <div class="p-header__menuLogo">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icn-twitter.svg" alt="">
+                </div>
+                <p class="p-header__menuText">修理メニュー</p>
+              </a>
+            </li>
+            <li class="p-header__menuItem">
+              <a class="p-header__menuLink" href="">
+                <div class="p-header__menuLogo">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icn-twitter.svg" alt="">
+                </div>
+                <p class="p-header__menuText">症状で選ぶ</p>
+              </a>
+            </li>
+            <li class="p-header__menuItem">
+              <a class="p-header__menuLink" href="">
+                <div class="p-header__menuLogo">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icn-twitter.svg" alt="">
+                </div>
+                <p class="p-header__menuText">お客様の声</p>
+              </a>
+            </li>
+            <li class="p-header__menuItem p-header__drawerMenu">
+              <div class="p-header__drawerMenuItem" href="">
+                <div class="p-header__menuLogo">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icn-twitter.svg" alt="">
+                </div>
+                <p class="p-header__menuText">店舗一覧</p>
+              </div>
+            </li>
+            <ul class="p-header__shopList">
+              <li class="p-header__shopItem">
+                <a href="">北海道・東北</a>
+              </li>
+              <li class="p-header__shopItem">
+                <a href="">関東</a>
+              </li>
+              <li class="p-header__shopItem">
+                <a href="">東海・北陸</a>
+              </li>
+              <li class="p-header__shopItem">
+                <a href="">関西</a>
+              </li>
+              <li class="p-header__shopItem">
+                <a href="">四国・中国</a>
+              </li>
+              <li class="p-header__shopItem">
+                <a href="">九州・沖縄</a>
+              </li>
+            </ul>
+            <li class="p-header__menuItem">
+              <a class="p-header__menuLink" href="">
+                <div class="p-header__menuLogo">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icn-twitter.svg" alt="">
+                </div>
+                <p class="p-header__menuText">加盟店募集</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main class="l-main">
---
+++ hero.php
@@ -0,0 +1,4 @@
+<section class="c-hero">
+  <div class="c-hero__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
+  <h2 class="c-hero__title">初めての方へ</h2>
+</section>
