b83f36bc64e680e25482e2a555fdd1a3913769ac
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2023-01-14 10:42:09 +0900

    hero.php修正

--- hero.php
+++ hero.php
@@ -1,4 +1,4 @@
 <section class="c-hero">
   <div class="c-hero__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
-  <h2 class="c-hero__title">初めての方へ</h2>
+  <h2 class="c-hero__title"><?php the_title(); ?></h2>
 </section>
