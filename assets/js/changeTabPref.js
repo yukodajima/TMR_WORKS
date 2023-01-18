d8e82247cf561e7741a6aba9f64ea5c7e84636a5
yukodajima <koda12131107@gmail.com>
2023-01-18 18:18:10 +0900

    changeTabPref.js上げ忘れ

--- assets/js/changeTabPref.js
+++ assets/js/changeTabPref.js
@@ -1,9 +1,8 @@
 $(window).on('load', function () {
   jQuery(function ($) {
-    const changeTabPref = $('.p-shopList__areaItem').on('click', function () {
-      alert('O');
-      // $(this).next().slideToggle();
-      // $(this).children().toggleClass('open');
-    });
+    const changeTabPref = $('.p-shopList__areaItem').on(
+      'click',
+      function () {}
+    );
   });
 });
