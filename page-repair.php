9e6e0ff55f9ed2f8f93ed4a3395a752edba69581
yukodajima <koda12131107@gmail.com>
2023-01-23 16:41:59 +0900

    page-rapair.phpタブ削除、p-menu__imgContainer直下にaタグ追加

--- page-repair.php
+++ page-repair.php
@@ -9,67 +9,74 @@
       </div>
     </div>
     <p class="p-menu__topText">下記メニューよりお選びください</p>
-    <div class="p-menu__container">
-      <div class="p-menu__textContainer orange">
-        <h2 class="p-menu__title white">修理メニュー</h2>
-        <h3 class="p-menu__subTitle white">Menu</h3>
-      </div>
-      <div class="p-menu__textContainer">
-        <h2 class="p-menu__title">症状で選択</h2>
-        <h3 class="p-menu__subTitle">Symptoms</h3>
-      </div>
-    </div>
     <ul class="p-menu__list show">
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">画面修理</p>
+          </a>
         </div>
-        <p class="p-menu__text">画面修理</p>
       </li>
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">
+              HDD・SSDの増設<br />
+              交換（OS再インストール）
+            </p>
+          </a>
         </div>
-        <p class="p-menu__text">
-          HDD・SSDの増設<br />
-          交換（OS再インストール）
-        </p>
       </li>
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">メンテナンス・初期化</p>
+          </a>
         </div>
-        <p class="p-menu__text">メンテナンス・初期化</p>
       </li>
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">キーボード修理</p>
+          </a>
         </div>
-        <p class="p-menu__text">キーボード修理</p>
       </li>
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">バッテリー交換</p>
+          </a>
         </div>
-        <p class="p-menu__text">バッテリー交換</p>
       </li>
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">データ復旧</p>
+          </a>
         </div>
-        <p class="p-menu__text">データ復旧</p>
       </li>
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">データ移行</p>
+          </a>
         </div>
-        <p class="p-menu__text">データ移行</p>
       </li>
       <li class="p-menu__item">
         <div class="p-menu__imgContainer">
-          <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+          <a href="<?php echo home_url("/repairDetail"); ?>">
+            <img class="p-menu__image" src="<?php echo get_template_directory_uri(); ?>//assets/images/logo-sample.png" alt="" />
+            <p class="p-menu__text">その他</p>
+
+          </a>
         </div>
-        <p class="p-menu__text">その他</p>
       </li>
     </ul>
     <ul class="p-menu__list">
