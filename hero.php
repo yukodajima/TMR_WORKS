50c2542ad97d4d87210bd9f9e90de996333c7cc2
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2023-01-26 14:05:24 +0900

    hero画像修正

--- hero.php
+++ hero.php
@@ -1,4 +1,5 @@
 <section class="c-hero">
+  <div class="c-hero__backGround"></div>
   <div class="c-hero__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
   <h2 class="c-hero__title"><?php the_title(); ?></h2>
 </section>
