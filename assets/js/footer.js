83596ade4f532e24f83a3f041a45c703e34a0c95
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2023-03-10 14:06:55 +0900

    ボーダー・画像修正

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,33 +1,33 @@
-$(window).on("load", function(){
-  jQuery(function($){
-    const $trigger = $(".p-footer__trigger")
-    const $shopList = $(".p-footer__shopList")
-    const $shopItem = $(".p-footer__shopItem")
-    var $windowWidth = $(this).width()
-    var $bp = 900
+// $(window).on("load", function(){
+//   jQuery(function($){
+//     const $trigger = $(".p-footer__trigger")
+//     const $shopList = $(".p-footer__shopList")
+//     const $shopItem = $(".p-footer__shopItem")
+//     var $windowWidth = $(this).width()
+//     var $bp = 900
 
-    if($windowWidth > $bp){
-      $shopItem.show()
-    }
-    $shopList.on("click", function () {
-      if (window.matchMedia("(max-width: 900px)").matches) {
-        $trigger.toggleClass('open');
-        $shopItem.slideToggle();
-      }
-    })
-    $(window).resize(function(){
-      var $windowWidth = $(this).width()
-      var $bp = 900
+//     if($windowWidth > $bp){
+//       $shopItem.show()
+//     }
+//     $shopList.on("click", function () {
+//       if (window.matchMedia("(max-width: 900px)").matches) {
+//         $trigger.toggleClass('open');
+//         $shopItem.slideToggle();
+//       }
+//     })
+//     $(window).resize(function(){
+//       var $windowWidth = $(this).width()
+//       var $bp = 900
 
-      if($windowWidth > $bp){
-        $shopItem.show()
-        $trigger.removeClass("open")
-      } else {
-        $shopItem.hide()
-        $trigger.removeClass("open")
-      }
-    })
+//       if($windowWidth > $bp){
+//         $shopItem.show()
+//         $trigger.removeClass("open")
+//       } else {
+//         $shopItem.hide()
+//         $trigger.removeClass("open")
+//       }
+//     })
 
 
-  })
-})
+//   })
+// })
