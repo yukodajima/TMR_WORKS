a537c0312899ab14d6a2287f61cb35f01119e23a
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-05-16 14:22:27 +0900

    サイトマップ修正

--- page-sitemap.php
+++ page-sitemap.php
@@ -1,3 +1,7 @@
 <?php get_header(); ?>
-<?php the_content(); ?>
+<section class="p-sitemap">
+  <div class="l-container">
+      <?php the_content(); ?>
+  </div>
+</section>
 <?php get_footer(); ?>
