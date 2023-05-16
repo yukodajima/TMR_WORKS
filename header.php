154c31a197fb753a7c5ce12c72ed3ac0801b23d8
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-05-16 15:26:13 +0900

    ページタイトル修正

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
+  <title><?php bloginfo('name'); wp_title('|', true, 'left'); ?></title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css" />
   <?php wp_head(); ?>
 </head>
