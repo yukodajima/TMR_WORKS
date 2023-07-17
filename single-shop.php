cace3a3336bec7cfe2af2ee8878ebbfdfbdfbb1b
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-07-17 19:00:40 +0900

    店舗詳細カスタムフィールド修正

--- single-shop.php
+++ single-shop.php
@@ -89,25 +89,25 @@
         </p>
         <p class="p-shopIntroduction__title">修理方法</p>
         <p>
-        <?php
-          $values = CFS()->get( 'repair_select' );
+          <?php
+          $values = CFS()->get('repair_select');
           $selects = array(); // $selects配列を初期化する
-          foreach ( $values as $key => $label ) {
+          foreach ($values as $key => $label) {
             $selects[] = $label; // $labelを$selects配列に追加する
           }
-          echo implode( '&nbsp;/&nbsp;', $selects ); // $selectsをカンマ区切りで出力する
-        ?>
+          echo implode('&nbsp;/&nbsp;', $selects); // $selectsをカンマ区切りで出力する
+          ?>
         </p>
         <p class="p-shopIntroduction__title">支払い方法</p>
         <p>
-        <?php
-          $values = CFS()->get( 'pay_select' );
+          <?php
+          $values = CFS()->get('pay_select');
           $selects = array(); // $selects配列を初期化する
-          foreach ( $values as $key => $label ) {
+          foreach ($values as $key => $label) {
             $selects[] = $label; // $labelを$selects配列に追加する
           }
-          echo implode( '&nbsp;/&nbsp;', $selects ); // $selectsをカンマ区切りで出力する
-        ?>
+          echo implode('&nbsp;/&nbsp;', $selects); // $selectsをカンマ区切りで出力する
+          ?>
         </p>
       </div>
     </div>
@@ -129,18 +129,22 @@
         </tr>
       </thead>
       <tbody>
-      <?php $fields01 = CFS()->get( 'price_list' ); ?>
-        <?php if ($fields01)://faq_loop が存在するとき ?>
-          <?php foreach ( $fields01 as $field ):?>
+        <?php $fields01 = CFS()->get('price_list'); ?>
+        <?php if ($fields01) : //faq_loop が存在するとき 
+        ?>
+          <?php foreach ($fields01 as $field) : ?>
             <tr>
               <td>
-                <?php echo $field['price_name'];//faq_loop の faq_qを表示 ?>
+                <?php echo $field['price_name']; //faq_loop の faq_qを表示 
+                ?>
               </td>
               <td>
-                <?php echo $field['price'];//faq_loop の faq_aを表示 ?>
+                <?php echo $field['price']; //faq_loop の faq_aを表示 
+                ?>
               </td>
               <td>
-                <?php echo $field['period'];//faq_loop の faq_aを表示 ?>
+                <?php echo $field['period']; //faq_loop の faq_aを表示 
+                ?>
               </td>
             </tr>
           <?php endforeach; ?>
@@ -159,41 +163,23 @@
       </div>
     </div>
     <ul class="p-shopRoot__list">
-      <li class="p-shopRoot__item">
-        <div class="p-shopRoot__itemImg">
-          <img src="<?php echo $cfs->get('root_img1'); ?>" alt="">
-        </div>
-        <p>
-          <?php echo get_post_meta($post->ID, 'root_text1', true); ?>
-        </p>
-      </li>
-      <li class="p-shopRoot__item">
-        <div class="p-shopRoot__itemImg">
-          <img src="<?php echo $cfs->get('root_img2'); ?>" alt="">
-        </div>
-        <p>
-          <?php echo get_post_meta($post->ID, 'root_text2', true); ?>
-        </p>
-      </li>
-      <li class="p-shopRoot__item">
-        <div class="p-shopRoot__itemImg">
-          <img src="<?php echo $cfs->get('root_img3'); ?>" alt="">
-        </div>
-        <p>
-          <?php echo get_post_meta($post->ID, 'root_text3', true); ?>
-        </p>
-      </li>
-      <li class="p-shopRoot__item">
-        <div class="p-shopRoot__itemImg">
-          <img src="<?php echo $cfs->get('root_img4'); ?>" alt="">
-        </div>
-        <p>
-          <?php echo get_post_meta($post->ID, 'root_text4', true); ?>
-        </p>
-      </li>
+      <?php $fields01 = CFS()->get('root_list'); ?>
+      <?php if ($fields01) : //faq_loop が存在するとき
+      ?>
+        <?php foreach ($fields01 as $field) : ?>
+          <li class="p-shopRoot__item">
+            <div class="p-shopRoot__itemImg">
+              <img src="<?php echo $cfs->get('root_img1'); ?>" alt="">
+            </div>
+            <p>
+              <?php echo get_post_meta($post->ID, 'root_text1', true); ?>
+            </p>
+          </li>
+        <?php endforeach; ?>
+      <?php endif; ?>
     </ul>
     <div class="p-shopRoot__itemMap">
-    <?php the_content(); ?>
+      <?php the_content(); ?>
     </div>
   </div>
 </section>
@@ -208,59 +194,60 @@
     <ul class="p-shopNews__list">
       <?php // 現在表示されている投稿と同じタームに分類された投稿を取得
       //閲覧している記事（投稿）に付与されているタグと同じ記事を表示
-  $taxonomy_slug = 'custom_tags'; // タクソノミーのスラッグを指定
-  $post_type_slug = 'news'; // 投稿タイプのスラッグを指定
-  $post_terms = wp_get_object_terms($post->ID, $taxonomy_slug); // タクソノミーの指定
-  if( $post_terms && !is_wp_error($post_terms)) { // 値があるときに作動
-    $terms_slug = array(); // 配列のセット
-    foreach( $post_terms as $value ){ // 配列の作成
-      $terms_slug[] = $value->slug; // タームのスラッグを配列に追加
-    }
-  }
-  $args = array(
-    'post_type' => $post_type_slug, // 投稿タイプを指定
-    'posts_per_page' => 5, // 表示件数を指定
-    'orderby' =>  'date', // ランダムに投稿を取得
-    'post__not_in' => array($post->ID), // 現在の投稿を除外
-    'order' => 'DESC',
-    'tax_query' => array( // タクソノミーパラメーターを使用
-      array(
-        'taxonomy' => $taxonomy_slug, // タームを取得タクソノミーを指定
-        'field' => 'slug', // スラッグに一致するタームを返す
-        'terms' => $terms_slug // タームの配列を指定
-      )
-    )
-  );
-  $the_query = new WP_Query($args); if($the_query->have_posts()):
-?>
-<?php while ($the_query->have_posts()): $the_query->the_post(); ?>
-<li class="p-shopNews__item">
-          <a href="<?php the_permalink() ?>">
-            <p class="p-shopNews__itemDate"><?php the_time("Y-m-d"); ?></p>
-            <p class="p-shopNews__itemContent">
-              &lt;
-              <?php
+      $taxonomy_slug = 'custom_tags'; // タクソノミーのスラッグを指定
+      $post_type_slug = 'news'; // 投稿タイプのスラッグを指定
+      $post_terms = wp_get_object_terms($post->ID, $taxonomy_slug); // タクソノミーの指定
+      if ($post_terms && !is_wp_error($post_terms)) { // 値があるときに作動
+        $terms_slug = array(); // 配列のセット
+        foreach ($post_terms as $value) { // 配列の作成
+          $terms_slug[] = $value->slug; // タームのスラッグを配列に追加
+        }
+      }
+      $args = array(
+        'post_type' => $post_type_slug, // 投稿タイプを指定
+        'posts_per_page' => 5, // 表示件数を指定
+        'orderby' =>  'date', // ランダムに投稿を取得
+        'post__not_in' => array($post->ID), // 現在の投稿を除外
+        'order' => 'DESC',
+        'tax_query' => array( // タクソノミーパラメーターを使用
+          array(
+            'taxonomy' => $taxonomy_slug, // タームを取得タクソノミーを指定
+            'field' => 'slug', // スラッグに一致するタームを返す
+            'terms' => $terms_slug // タームの配列を指定
+          )
+        )
+      );
+      $the_query = new WP_Query($args);
+      if ($the_query->have_posts()) :
+      ?>
+        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+          <li class="p-shopNews__item">
+            <a href="<?php the_permalink() ?>">
+              <p class="p-shopNews__itemDate"><?php the_time("Y-m-d"); ?></p>
+              <p class="p-shopNews__itemContent">
+                &lt;
+                <?php
                 $taxonomy = 'custom_tags'; // タグのタクソノミー名
                 $tags = get_the_terms(get_the_ID(), $taxonomy);
 
                 if ($tags && !is_wp_error($tags)) {
                   $tag_names = array();
                   foreach ($tags as $tag) {
-                      $tag_names[] = $tag->name;
+                    $tag_names[] = $tag->name;
                   }
                   echo implode(' / ', $tag_names);
                 }
                 ?>
-              &gt;
-              <?php the_title(); ?>
-            </p>
-          </a>
-        </li>
-<?php endwhile; ?>
-<?php wp_reset_postdata(); ?>
-<?php else: ?>
-  <p class="p-shopList__text">お知らせはありません</p>
-<?php endif; ?>
+                &gt;
+                <?php the_title(); ?>
+              </p>
+            </a>
+          </li>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+      <?php else : ?>
+        <p class="p-shopList__text">お知らせはありません</p>
+      <?php endif; ?>
     </ul>
   </div>
 </section>
