e22fddaecde4be0a646d4e8041f356cd43aa14e3
sakifukumoto <saki.fukumoto9918@gmail.com>
2024-04-20 11:27:06 +0900

    ブログ一覧ページネーション追加

--- archive-blog.php
+++ archive-blog.php
@@ -4,11 +4,14 @@
   <div class="l-container">
     <ul class="p-blogList__list">
       <?php
-        $args = array(
-          'post_type' => 'blog', // 投稿タイプを指定
-          'posts_per_page' => 10, // 表示する記事数
+        $pages = get_query_var('paged') ? get_query_var('paged') : 1;
+        $blogList_query = new WP_Query(
+          array(
+            'post_type' => 'blog', // 投稿タイプを指定
+            'posts_per_page' => 10, // 表示する記事数
+            'paged' => $paged, //ページネーション用
+          )
         );
-        $blogList_query = new WP_Query($args);
         if ($blogList_query->have_posts()) :
           while ($blogList_query->have_posts()) :
             $blogList_query->the_post();
@@ -37,6 +40,7 @@
       wp_reset_postdata();
       ?>
     </ul>
+    <?php wp_pagenavi(); ?>
   </div>
 </section>
 <?php get_footer(); ?>
